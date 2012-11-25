533ed315b5bd07f795ec80755ff3eb5cce7891db
louislam <louislam@7b21d1ba-8b47-4109-b1f7-872d274fe117>
2012-11-25 11:43:47 +0000

    git-svn-id: https://subversion.assembla.com/svn/hkepc-android@6 7b21d1ba-8b47-4109-b1f7-872d274fe117

--- HKEPC/gen/net/louislam/hkepc/R.java
+++ HKEPC/gen/net/louislam/hkepc/R.java
@@ -15,13 +15,17 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int About=0x7f070007;
+        public static final int About=0x7f07000b;
         public static final int container=0x7f070000;
-        public static final int login=0x7f070006;
+        public static final int login=0x7f070009;
         public static final int loginButton=0x7f070004;
-        public static final int menu_settings=0x7f070008;
+        public static final int logout=0x7f07000a;
+        public static final int menu_settings=0x7f07000c;
+        public static final int msg=0x7f070008;
         public static final int password=0x7f070003;
+        public static final int post=0x7f070006;
         public static final int refresh=0x7f070005;
+        public static final int reply=0x7f070007;
         public static final int username=0x7f070002;
         public static final int webView1=0x7f070001;
     }
